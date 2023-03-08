7ac0f3e02cac1ae7fb5c482bd6251715aa6ad640
cesar rodriguez <streamlink777@live.com>
2023-03-08 00:15:23 -0500

    add new expenses group

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
-import ExpenseItem from "./components/ExpenseItem";
+import Expenses from "./components/Expenses/Expenses";
+import NewExpense from "./components/NewExpense/NewExpense";
 
 function App() {
+
   return (
     <div>
-      <h2>Let's get started!</h2>
-      <ExpenseItem />
+      <NewExpense />
+      <Expenses />
     </div>
   );
 }
---
+++ src/components/NewExpense/ExpenseForm.js
@@ -0,0 +1,30 @@
+import React from "react";
+import './ExpenseForm.css'
+
+const ExpenseForm = ()=>{
+
+return(
+    <form>
+        <div className="new-expense__controls">
+            <div className="new-expense__control">
+                <label>Title</label>
+                <input type='Text' />
+            </div>
+            <div className="new-expense__control">
+                <label>amount</label>
+                <input type='number' min="0.01" step="0.01" />
+            </div>
+            <div className="new-expense__control">
+                <label>Date</label>
+                <input type='date' min="2019-01-01" max="2023-12-12" />
+            </div>
+
+            <div className="new_expense__actions">
+                <button>Add Expense</button>
+            </div>
+        </div>
+    </form>
+)
+}
+
+export default ExpenseForm;
