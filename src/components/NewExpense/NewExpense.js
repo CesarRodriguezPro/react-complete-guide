c208960973bd6291de251ba22698da0ebb47938d
cesar rodriguez <streamlink777@live.com>
2023-03-12 15:18:35 -0400

    start working with filter components

--- src/components/NewExpense/NewExpense.js
+++ src/components/NewExpense/NewExpense.js
@@ -5,21 +5,22 @@ import ExpenseForm from "./ExpenseForm";
 const NewExpense = (props) => {
 
   const saveExpenseDataHandler = (enteredExpenseData) => {
+    
     const expenseData = {
       ...enteredExpenseData,
       id: Math.random().toString(),
     };
+    
     props.onAddExpense(expenseData);
   }
 
 
   return ( 
-  <div className = "new-expense" >
-    <
-    ExpenseForm onSaveExpenseData = {
-      saveExpenseDataHandler
-    }
-    /> 
+    <div className = "new-expense" >
+      <ExpenseForm onSaveExpenseData = {
+        saveExpenseDataHandler
+      }
+      /> 
     </div>
   )
 }
